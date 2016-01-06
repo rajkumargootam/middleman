5e991e99fcdda0ecd0684c2647fdb84d6afef1ea
rajkumar <rajkumar.gootam@gmail.com>
2016-01-06 22:01:23 +0530

    Separate layouts for landing page and other pages

--- config.rb
+++ config.rb
@@ -9,6 +9,10 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# Pretty URLs
+activate :directory_indexes
+
+
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
